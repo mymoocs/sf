132ff50eaebbfcd93231f25e129688a03ad83c16
Arthur <artie.vard@gmail.com>
2018-10-20 23:56:17 +0400

    re-structure, solve one more ex.

--- LF/Basics/bools.v
+++ LF/Basics/bools.v
@@ -59,3 +59,22 @@ Example test_nandb3: (nandb false true) = true.
 Proof. simpl. reflexivity. Qed.
 Example test_nandb4: (nandb true true) = false.
 Proof. simpl. reflexivity. Qed.
+
+
+(* Exercise: 1 star (andb3) *)
+(* Do the same for the andb3 function below. This function should return true when all of its inputs are true, and false otherwise.*)
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+  match b1 with 
+  | false => false
+  | true  => (andb  b2 b3)
+end.
+  
+Example test_andb31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
